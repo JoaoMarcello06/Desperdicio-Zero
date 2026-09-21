2792eb91a43d56700d8a76eca40d1be1a47c9c6c
João Marcello <fagundesjoaomarcello@gmail.com>
2026-09-21 10:48:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icon-192.png'
+];
+
+// Instalar e guardar ficheiros em cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
+// Interceptar pedidos para carregar do cache se estiver offline
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
